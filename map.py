cb16eac0e61fa6d964f8fa7c2f0a6b209928eda5
Henry <32826270+hcm444@users.noreply.github.com>
2023-04-16 17:50:24 -0700

    Update map.py
    OOP

--- map.py
+++ map.py
@@ -2,32 +2,32 @@ import sqlite3
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-# Connect to the SQLite3 database
-conn = sqlite3.connect('aircraft_positions.db')
 
-# Load the GeoDataFrame containing the map
-gdf = gpd.read_file("gadm41_BLR_shp/gadm41_BLR_0.shp")
+class FlightMap:
+    def __init__(self, db_filename, shapefile):
+        self.conn = sqlite3.connect(db_filename)
+        self.gdf = gpd.read_file(shapefile)
+        self.fig, self.ax = plt.subplots(figsize=(10, 10))
 
-# Set up the plot
-fig, ax = plt.subplots(figsize=(10, 10))
-gdf.plot(ax=ax, edgecolor='black', facecolor='none')
+    def plot_routes(self):
+        self.gdf.plot(ax=self.ax, edgecolor='black', facecolor='none')
+        c = self.conn.cursor()
+        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name LIKE 'aircraft_%'")
+        tables = c.fetchall()
+        for table in tables:
+            table_name = table[0]
+            c.execute(f"SELECT latitude, longitude FROM {table_name}")
+            rows = c.fetchall()
+            if len(rows) > 1:
+                lats, lons = zip(*rows)
+                self.ax.plot(lons, lats, label=table_name)
+        self.ax.legend(loc='upper left')
+        self.ax.set_title('Flight Routes')
 
-# Query the database for all tables that start with "aircraft_"
-c = conn.cursor()
-c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name LIKE 'aircraft_%'")
-tables = c.fetchall()
+    def save(self, filename, dpi=300):
+        plt.savefig(filename, dpi=dpi)
 
-# Loop through each table and plot the flight route
-for table in tables:
-    table_name = table[0]
-    c.execute(f"SELECT latitude, longitude FROM {table_name}")
-    rows = c.fetchall()
-    if len(rows) > 1:
-        lats, lons = zip(*rows)
-        ax.plot(lons, lats, label=table_name)
 
-# Add a legend and title to the plot
-ax.legend(loc='upper left')
-ax.set_title('Flight Routes')
-
-plt.savefig(f"map.png", dpi=300)
+flight_map = FlightMap('aircraft_positions.db', 'gadm41_BLR_shp/gadm41_BLR_0.shp')
+flight_map.plot_routes()
+flight_map.save('map.png')
